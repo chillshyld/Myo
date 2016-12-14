46fc3c53f5508214c6159394051d6b2174dfe2a7
Chieh <chill_shyld@hotmail.com>
2016-12-14 16:21:06 +0700

    Initial Commit

---
+++ Myo3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Myo3
+//
+//  Created by Suradid Chao on 12/14/2559 BE.
+//  Copyright © 2559 Suradid Chao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
